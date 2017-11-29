3fa805b03c6a57898afd47ba3b20d229c8819197
Roberto Di Remigio <roberto.diremigio@gmail.com>
2017-11-28 19:21:47 -0500

    Update Ruby dep

---
+++ default.nix
@@ -0,0 +1,16 @@
+let
+  hostPkgs = import <nixpkgs> {};
+  nixpkgs = (hostPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "ac355040656de04f59406ba2380a96f4124ebdad";
+    sha256 = "0frhc7mnx88sird6ipp6578k5badibsl0jfa22ab9w6qrb88j825";
+  });
+in
+  with import nixpkgs {};
+    stdenv.mkDerivation {
+      name = "flaks-website";
+      buildInputs = [
+        bundler
+      ];
+    }
